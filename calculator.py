d05db2ad5ac43ad27345ad0ef7dd574e0c7b6caf
gtsofa <jlsnyule@yahoo.com>
2017-10-22 23:27:20 +0300

    debug-ged with print statements

--- calculator.py
+++ calculator.py
@@ -1,3 +1,12 @@
 class Calculator(object):
     def add(self, a, b):
-        return a + b
+        number_types = (int, float, complex)
+
+        if isinstance(a, number_types) and isinstance(b, number_types):
+            print ('A is: {}'.format(a))
+            print ('B is: {}'.format(b))
+            result =  a - b
+            print ('Result is: {}'.format(result))
+            return result
+        else:
+            raise ValueError
--- test_calculator.py
+++ test_calculator.py
@@ -3,10 +3,19 @@ import unittest
 from calculator import Calculator
 
 class TddInPythonExample(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
     def test_add_method_returns_correct_results(self):
-        calc = Calculator()
-        result = calc.add(2,2)
+        result = self.calc.add(2,2)
         self.assertEqual(4, result)
+
+    def test_calculator_returns_error_message_if_both_args_not_numbers(self):
+        self.assertRaises(ValueError, self.calc.add, 'two', 'three')
+
+    def test_calculator_returns_error_message_if_a_arg_not_number(self):
+        self.assertRaises(ValueError, self.calc.add, 'two', 3)
+    def test_calculator_returns_error_message_if_b_arg_not_number(self):
+        self.assertRaises(ValueError, self.calc.add, 2, 'three')
 
 if __name__ == '__main__':
     unittest.main()
