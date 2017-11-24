41520c151c2e6c3d36a92232ed0737131178f894
gtsofa <jlsnyule@yahoo.com>
2017-11-24 22:55:01 +0300

    refactor pizzas

--- pizzas.py
+++ pizzas.py
@@ -1,4 +1,5 @@
 pizzas = ['neopolitan', 'chicago', 'silician', 'california', 'tomatopie']
 for pizza in pizzas:
  print("I, Mr. Tsofa.. like "+ pizza.title() + " pizza.")
-
+print("I love pizza so much, i cannot go a meal with them.")
+print("i really love pizza! \n")
