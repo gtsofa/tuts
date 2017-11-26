90bb31ac29d8f3225f36fae78d65dd44acca26e5
gtsofa <jlsnyule@yahoo.com>
2017-11-26 16:25:49 +0300

    modify conditionaltests

--- conditionalTests.py
+++ conditionalTests.py
@@ -1,1 +1,10 @@
 #5-1: Conditional tests:
+"""
+Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. Your code
+should look something like this:
+"""
+car = 'subaru'
+if car == 'subaru':
+    print("Is car == 'subaru'? I predicted True.")
+    
