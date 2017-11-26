9723ea758d3f54f87b91f160203c77eba01ee241
gtsofa <jlsnyule@yahoo.com>
2017-11-26 23:13:29 +0300

    add implementation code

--- languages.py
+++ languages.py
@@ -1,2 +1,11 @@
 #using a set - to make elements in a set be unique.
 
+favourite_languages = {
+    'julius' : 'c#',
+    'daktari' : 'python',
+    'peter' : 'php',
+    'eunice' : 'ruby',
+}
+print("The following languages have been mentioned:")
+for language in favourite_languages.values():
+    print(language.title())
