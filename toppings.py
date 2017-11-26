a7bd735ff98e0264ead2ebd3f83212b162cec98a
gtsofa <jlsnyule@yahoo.com>
2017-11-26 18:26:32 +0300

    [feature] we add available topping list

--- toppings.py
+++ toppings.py
@@ -1,12 +1,23 @@
 #requested toppings.
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+# for requested_topping in requested_toppings:
+#     if requested_topping == 'green peppers':
+#         print("Sorry, we are out of green peppers right now.")
+#     else:
+
+#         print("Adding " + requested_topping + ".")
+
+# print("\nFinished making your pizza.")
+
+#multiple lists
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
 
 for requested_topping in requested_toppings:
-    if requested_topping == 'green peppers':
-        print("Sorry, we are out of green peppers right now.")
-    else:
-
+    if requested_topping in available_toppings:
         print("Adding " + requested_topping + ".")
+    else:
+        print("Sorrry, we don't have " + requested_topping + ".")
 
 print("\nFinished making your pizza.")
-
