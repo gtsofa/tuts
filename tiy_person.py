5151630eacace77a7955873b8ef7e9eb7ede3f21
gtsofa <jlsnyule@yahoo.com>
2017-11-26 22:53:29 +0300

    person implementation

--- tiy_person.py
+++ tiy_person.py
@@ -1,2 +1,8 @@
 #dictionary containing person information
+person = {'first_name':'julis', 'last_name':'tsofa', 'age':29, 'city':'watamu'}
+print(person)
+print("Your first name is "+ person['first_name'].title())
+for key, value in person.items():
+    print("\nKey: " + key)
+    print("Value: " + str(value))
 
