7a5c75ec31e62d72c7e2e334a690403404ef42ac
gtsofa <jlsnyule@yahoo.com>
2017-11-26 17:46:56 +0300

    refactor

--- alien_color.py
+++ alien_color.py
@@ -1,2 +1,12 @@
 #Try it yourself
 #5-3. Alien Colors #1
+#this passes:
+# alien_color = 'green'
+
+# if alien_color == 'green':
+#     print("you have earned 5 points.")
+
+#version that pass & fails
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("you have earned 5 points.")
