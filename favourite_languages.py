5fa5adb66726f2e3175274dd1dc097e9364ffaa4
gtsofa <jlsnyule@yahoo.com>
2017-11-28 14:02:56 +0300

    refactor

--- favourite_languages.py
+++ favourite_languages.py
@@ -1,2 +1,7 @@
 #dictionary containing a list
-
+favourite_languages = {
+    'julius': ['c#', 'ruby', 'python'],
+    'sarah': 'statistics',
+    'daktari':['java', 'python'],
+    'peter': ['php', 'photoshop', 'python'],
+    }
