3c41440fdd1c5a869af4af3f8313eee60b884bba
gtsofa <jlsnyule@yahoo.com>
2017-11-28 16:16:50 +0300

    refactor by adding if stmt to check if person has more than one fav language

--- favourite_languages.py
+++ favourite_languages.py
@@ -7,8 +7,12 @@ favourite_languages = {
     }
 
 for name, languages in favourite_languages.items():
-    print("\n" + name.title() +"'s favourite languages are.")
-    
+    if len(languages) <= 1:
+        print(name.title() + "'s favourite language is:")
+        
+    else:
+        print("\n" + name.title() +"'s favourite languages are:")
+     
     for language in languages:
         print(language.title())
         
