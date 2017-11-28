bfb28dd0a0444c092697cf74ceeca79e9a7142e6
gtsofa <jlsnyule@yahoo.com>
2017-11-28 16:59:08 +0300

    refactor the code to make it readable

--- many_users.py
+++ many_users.py
@@ -1,1 +1,31 @@
 #nesting a dictionary in a dictionary.
+users = {
+    'maestro': {
+        'firstname': 'tsofa',
+        'lastname': 'nyule',
+        'location': 'watamu',
+    },
+    'smunga': {
+        'firstname': 'sarah',
+        'lastname': 'munga',
+        'location': 'dam estate',
+    },
+    'daktari': {
+        'firstname': 'james',
+        'lastname': 'dindi',
+        'location': 'busia',
+    },
+
+}
+
+for username, user_info in users.items():
+    print("\n Username: " + username)
+    #print("Full name: " + user_info['firstname'].title() +" " + user_info['lastname'].title())
+    #refactor code
+    full_name = user_info['firstname'] + " " + user_info['lastname']
+    print("Full name: " + full_name.title())
+    #refactor code to make it readable
+    #print("Location: " + user_info['location'].title())
+    location = user_info['location']
+    print("Location: " + location.title())
+
