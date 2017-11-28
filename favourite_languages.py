99ef903ba18a7adb18e4564e83d428a071c8f559
gtsofa <jlsnyule@yahoo.com>
2017-11-28 16:06:49 +0300

    commit changes

--- favourite_languages.py
+++ favourite_languages.py
@@ -1,7 +1,16 @@
 #dictionary containing a list
 favourite_languages = {
     'julius': ['c#', 'ruby', 'python'],
-    'sarah': 'statistics',
+    'sarah': ['statistics'],
     'daktari':['java', 'python'],
     'peter': ['php', 'photoshop', 'python'],
     }
+
+for name, languages in favourite_languages.items():
+    print("\n" + name.title() +"'s favourite languages are.")
+    
+    for language in languages:
+        print(language.title())
+        
+
+
