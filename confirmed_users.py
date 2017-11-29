8258a6c9ed59ca1eb158d8d1bc118efdcfa1bee2
gtsofa <jlsnyule@yahoo.com>
2017-11-29 18:57:52 +0300

    refactor

--- confirmed_users.py
+++ confirmed_users.py
@@ -1,1 +1,14 @@
 #website users
+unconfirmed_users = ['tsofa', 'giulio', 'peter', 'sarah']
+confirmed_users = []
+
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+
+    print("Verifying current users: " + current_user.title())
+    confirmed_users.append(current_user)
+
+#Displaying confirmed users
+print("The following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
