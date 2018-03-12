3711a9f30d114b5a1220d9ed8e7e666c83ecdec3
gtsofa <jlsnyule@yahoo.com>
2018-03-12 21:40:23 +0300

    changes made

--- dog.py
+++ dog.py
@@ -6,6 +6,7 @@ a simple attempt to model a dog.
 class Dog():
 
     def __init__(self, name, age):
+        """Initialize name and age attributes."""
         self.name = name
         self.age = age
 
