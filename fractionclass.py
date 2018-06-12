6532a0c060dc7b407cebe9b0eef5c2cfe1bbfe17
gtsofa <jlsnyule@yahoo.com>
2018-06-12 23:12:16 +0300

    add changes made

---
+++ fractionclass.py
@@ -0,0 +1,11 @@
+# class that model a Fraction
+
+class Fraction:
+
+    def __init__(self, num, deno):
+        self.num = num
+        self.deno = deno
+
+myfraction = Fraction(3,5)
+
+print(myfraction)
