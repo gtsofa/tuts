a0acb0f13b2fdbccab0cf751421e7feea1834f16
Julius Nyule <jlsnyule@yahoo.com>
2018-06-29 08:24:11 +0300

    add implement code for linear search

--- linear_search.py
+++ linear_search.py
@@ -1,1 +1,17 @@
-
+def linear_search(list_values, target_item, start=0):
+  """
+  method to implement linear search
+    -liste_values[] => list with all the values
+    - target_item => value to be found
+    - n => total number of elements in the array
+  """
+  for i in range(start, len(list_values)):
+    if list_values[i] == target_item:
+      return i
+    
+    return -1
+ 
+# test the implementation
+list_values = [5, 34, 65, 12, 77, 35]
+target_item = 77
+print(linear_search(list_values,target_item))
